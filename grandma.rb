f6cfda2ec91534abdc638eb1b0e102b675d3bc11
Meisuleen Kased <meisuleen@gmail.com>
2018-10-31 17:40:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
-    return phrase.upcase
+  elsif phrase === phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
 
