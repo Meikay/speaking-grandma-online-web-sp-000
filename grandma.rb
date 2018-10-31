60f63ed3997981f18f9f8cd62b5dba9fa131d3b1
Meisuleen Kased <meisuleen@gmail.com>
2018-10-31 17:44:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase === phrase.upcase
-    puts "NO, NOT SINCE 1938!" || "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!" || "NO, NOT SINCE 1938!"
   end
 end
 
